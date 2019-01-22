e1f59c948df136ac4134283c4febea9ad1c3c6f1
parikjoshi <46922373+parikjoshi@users.noreply.github.com>
2019-01-22 15:27:24 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,37 @@
+abstract class Student
+{
+abstract void details();
+}
+class name extends Student
+{
+void details()
+{
+System.out.println("Student name is Parminder kour ");
+}
+}
+class Roll extends Student
+{
+void details()
+{
+System.out.println("Student Roll is 15222 ");
+}
+}
+class course extends Student
+{
+void details()
+{
+System.out.println("Student course is Android ");
+}
+}
+class test
+{
+public static void main(String []args)
+{
+Student s=new course();
+s.details();
+Student s1=new name();
+s1.details();
+Student s2=new  Roll();
+s2.details();
+}
+}
